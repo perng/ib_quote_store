16e2f72b92308c1ab6a0260d6c25f713c2e25058
Charles Perng <charles@perng.com>
2024-09-22 01:42:58 -0700

    get_quote done

--- create_table.sql
+++ create_table.sql
@@ -1,4 +1,5 @@
 CREATE TABLE option_data (
+    quote_type VARCHAR(10),
     symbol VARCHAR(10),
     expiration DATE,
     strike DECIMAL(10, 2),
@@ -11,5 +12,16 @@ CREATE TABLE option_data (
     volume INTEGER,
     average DECIMAL(10, 2),
     barCount INTEGER,
-    PRIMARY KEY (symbol, expiration, strike, right, date)
+    PRIMARY KEY (quote_type,symbol, expiration, strike, right, date)
 );
+
+CREATE TABLE quote_status (
+    quote_type VARCHAR(10),
+    symbol VARCHAR(10),
+    expiration DATE,
+    strike DECIMAL(10, 2),
+    right CHAR(1),
+    latest DATETIME,
+    PRIMARY KEY (quote_type, symbol, expiration, strike, right)
+);
+
